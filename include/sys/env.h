b6ce53739393c20100a1a02e677c1958e6999024
Glowman554 <glowman434@gmail.com>
2022-08-04 13:08:39 +0200

    remade arrow keys impl

--- include/sys/env.h
+++ include/sys/env.h
@@ -2,6 +2,8 @@
 #pragma once
 #include <extern.h>
 
+#include <stdbool.h>
+
 #define ENV_ARGV 0
 #define ENV_ENVP 1
 #define ENV_ERRNO 2
@@ -15,16 +17,34 @@
 #define ENV_READ_RUNNING_TASKS 10
 #define ENV_ACPI_REBOOT 11
 #define ENV_GET_LOADED_MODULES 12
+#define ENV_GET_SPECIAL_KEYS 13
 
 enum signal_handlers {
-	SIG_UP_ARROW = 33, // signals before signal 33 are exceptions
-	SIG_DOWN_ARROW,
-	SIG_LEFT_ARROW,
-	SIG_RIGHT_ARROW,
+	SIG_SPECIAL_KEY_UP = 33, // signals before signal 33 are exceptions
+	SIG_SPECIAL_KEY_DOWN,
 	SIG_INTR
 };
 
-        
+typedef struct special_keys_down {
+	bool left_shift;
+	bool left_ctrl;
+	bool left_alt;
+	bool left_gui;
+
+	bool right_shift;
+	bool right_ctrl;
+	bool right_alt;
+	bool right_gui;
+
+	bool caps_lock;
+	bool num_lock;
+	bool scroll_lock;
+
+	bool up_arrow;
+	bool down_arrow;
+	bool left_arrow;
+	bool right_arrow;
+} special_keys_down_t;
 
 EXPOSEC char** env(int mode);
 EXPOSEC void env2(int mode);
