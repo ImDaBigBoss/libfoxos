270017232e82c4389033c7755625c1050f76e539
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2022-09-20 18:37:14 +0200

    List update

--- include/foxos/list.h
+++ include/foxos/list.h
@@ -13,22 +13,60 @@ class list_t {
 		};
 
 		list_t() {
-			length = 0;
-			current_id = 0;
-            data = 0;
-		};
+			this->length = 0;
+			this->free_segments = 0;
+
+			this->current_id = 1;
+			this->data = 0;
+		}
+
+		list_t(int initial_length) {
+			this->length = initial_length;
+			this->free_segments = initial_length;
+
+			this->current_id = 1;
+			this->data = (node*) malloc(initial_length * sizeof(node));
+			memset(this->data, 0, initial_length * sizeof(node));
+		}
 
 		~list_t() {
-            if (data) {
-			    free(data);
-            }
+			if (this->data) {
+				free(this->data);
+			}
+		}
+
+		int add(T d) {
+			node* new_node;
+
+			if (this->free_segments > 0) {
+				for (int i = 0; i < this->length; i++) {
+					if (!this->data[i].id) { //Free segment found
+						new_node = &this->data[i];
+						this->free_segments--;
+						break;
+					}
+				}
+			} else {
+				this->length++;
+
+				this->data = (node*) realloc((void*) this->data, sizeof(node) * this->length);
+				new_node = &this->data[this->length - 1];
+			}
+
+			memset(new_node, 0, sizeof(node));
+			new_node->data = d;
+			new_node->id = this->current_id++;
+
+			return new_node->id;
 		}
 
 		template <typename Y>
 		node* find(bool (*f)(Y, node*), Y arg) {
-			for (size_t i = 0; i < length; i++) {
-				if (f(arg, &data[i])) {
-					return &data[i];
+			for (size_t i = 0; i < this->length; i++) {
+				if (this->data[i].id) { //Used segment
+					if (f(arg, &this->data[i])) {
+						return &this->data[i];
+					}
 				}
 			}
 			
@@ -36,47 +74,40 @@ class list_t {
 		}
 
 		void foreach(void (*f)(node*, void* lambda_data), void* lambda_data) {
-			for (size_t i = 0; i < length; i++) {
-				f(&data[i], lambda_data);
+			for (size_t i = 0; i < this->length; i++) {
+				if (this->data[i].id) { //Used segment
+					f(&this->data[i], lambda_data);
+				}
 			}
 		}
 
 		void foreach(void (*f)(node*)) {
-			for (size_t i = 0; i < length; i++) {
-				f(&data[i]);
+			for (size_t i = 0; i < this->length; i++) {
+				if (this->data[i].id) { //Used segment
+					f(&this->data[i]);
+				}
 			}
 		}
 
 		bool remove_index(int index) {
-			if (index < 0 || index >= length) {
+			if (index < 0 || index >= this->length) {
 				return false;
 			}
 
-			length--;
-			if (length == 0) { //If there are no more nodes, then just free the list
-				free(data);
-				data = 0;
-			} else { //If there are still nodes, then reallocate the list
-				node* new_data = (node*) malloc(sizeof(node) * length);
-
-				int current_id = 0;
-				for (int i = 0; i < length + 1; i++) {
-					if (i != index) {
-						new_data[current_id] = data[i];
-						current_id++;
-					}
-				}
-
-				free(data);
-				data = new_data;
+			if (!this->data[index].id) { //Free segment
+				return false;
 			}
+
+			memset(&this->data[index], 0, sizeof(node));
+
+			this->free_segments++;
 
 			return true;
 		}
 
 		bool remove(int id) {
-			for (size_t i = 0; i < length; i++) {
-				if (data[i].id == id) {
+			for (size_t i = 0; i < this->length; i++) {
+				if (this->data[i].id == id) {
 					return remove_index(i);
 				}
 			}
@@ -85,8 +116,8 @@ class list_t {
 		}
 
 		bool remove(node* n) {
-			for (size_t i = 0; i < length; i++) {
-				if (&data[i] == n) {
+			for (size_t i = 0; i < this->length; i++) {
+				if (&this->data[i] == n) {
 					return remove_index(i);
 				}
 			}
@@ -94,54 +125,32 @@ class list_t {
 			return false;
 		}
 
-		int add(T d, int index) {
-			length++;
-			if (index < 0 || index >= length) {
-				return -1;
-			}
-
-			data = (node*) realloc((void*) data, sizeof(node) * length);
-			if (!data) {
-				return -1;
-			}
-			
-			node* new_node;
-
-			if (index == length - 1) {
-				new_node = &data[index];
-			} else {
-				for (int i = index + 1; i < length; i++) {
-					data[i] = data[i - 1];
-				}
-
-				new_node = &data[index];
-			}
-
-			memset(new_node, 0, sizeof(node));
-			new_node->data = d;
-			new_node->id = current_id++;
-
-			return new_node->id;
+		int get_length() {
+			return this->length - this->free_segments;
 		}
-
-		int add(T d) {
-			return add(d, length); //Add to end
-		}
-
-        int get_length() {
-            return length;
-        }
 
 		node* get(int index) {
-			if (index < 0 || index >= length) {
+			if (index < 0 || index >= this->length - this->free_segments) {
 				return nullptr;
 			}
 
-			return &data[index];
+			int tmp = 0;
+			for (int i = 0; i < this->length; i++) {
+				if (this->data[i].id) { //Used segment
+					if (tmp == index) {
+						return &this->data[i];
+					}
+					tmp++;
+				}
+			}
+
+			return nullptr;
 		}
 	
-    private:
+	private:
 		int length;
+		int free_segments;
+
 		int current_id;
 		node* data;
 };
